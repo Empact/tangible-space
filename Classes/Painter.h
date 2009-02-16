871485e8ac1d5151d86d18fd3f618118044d83f7
Ben Woosley <ben.woosley@gmail.com>
2009-02-15 23:57:40 -0500

    Add a Painter class

---
+++ Classes/Painter.h
@@ -0,0 +1,16 @@
+//
+//  Painter.h
+//  TangibleSpace
+//
+//  Created by Ben on 2/15/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Painter : NSObject {
+
+}
+
+@end
---
+++ Classes/Painter.m
@@ -0,0 +1,14 @@
+//
+//  Painter.m
+//  TangibleSpace
+//
+//  Created by Ben on 2/15/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "Painter.h"
+
+
+@implementation Painter
+
+@end
